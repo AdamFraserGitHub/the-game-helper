33bc8042a1db0fffe8fcd4589821c82d6b4f8e31
Adam Fraser AHCS <adamfraserahcs@gmail.com>
2018-12-21 08:49:31 +0000

    commit

--- gameStrategies/player.py
+++ gameStrategies/player.py
@@ -20,7 +20,9 @@ class Square:
 def generateSquares():
     availableSquares = []
 
-    for i in range
+    for x in range(0,7):
+        for y in range(0,7):
+            availableSquares.append(Square(x,y))
 
     return availableSquares
 
@@ -28,20 +30,75 @@ class Player:
     positions = []
 
     def __init__(self):
-        self.squares = [None] * (7)
+        self.score = 0
+        self.bank = 0
+        self.block = False
+        self.reflect = False
+        self.dead = False
+
+        self.squares = []
+        for i in range(7):
+            self.squares.append([None] * 7)
 
     def generateChoices(self):
         availableSquares = generateSquares()
         items = genItems()
         
         for x in range(7):
-            self.squares[x] = [None] * 7
             for y in range(7):
-                randSquare = availableSquares.pop(randint(0,len(availableSquares)))
+                randSquare = availableSquares.pop(randint(0,len(availableSquares) - 1))
 
-                self.squares[randSquare.x][randSquare.y] = items.pop(0) #since we're removing one each time and get the randomness from the position on the grid
+                self.squares[randSquare.x][randSquare.y] = items.pop(0)#since we're removing one each time and get the randomness from the position on the grid
 
         print(self.squares)
+
+    def steal(self):
+        tempScore = self.score
+        self.score = 0
+        return tempScore
+
+    def kill(self):
+        self.dead = True
+        self.score = 0 #powerups?
+
+    def gift(self):
+        if(self.dead):
+            self.dead = False
+        else:
+            self.score += 1000
+
+    def snowBall(self):
+        self.score = 0
+    
+    def swap(self, scoreSwap):
+        scoreTemp = self.score
+        self.score = scoreSwap
+        return scoreTemp
+
+    def chooseNextSquare(self):
+        #?????
+        pass
+
+    def gainBlock(self):
+        self.block = True
+
+    def gainReflect(self):
+        self.reflect = True
+    
+    def resetTo0(self):
+        self.score = 0
+    
+    def doubleScore(self):
+        self.score *= 2
+
+    def bankScore(self):
+        self.bank = self.score
+        self.score = 0
+
+players = []
+for i in range()
+    
+
+
+
         
-p = Player()
-p.generateChoices()
